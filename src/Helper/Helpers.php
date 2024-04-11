f9f8becc2cf6ce35005f7d3cd5adec503beda5ad
Jenson.Wang <rebing0512@aliyun.com>
2024-04-11 15:35:27 +0800

    commit：获取城市简称和获取城市信息接口和方法封装，测试通过

--- src/Helper/Helpers.php
+++ src/Helper/Helpers.php
@@ -3,8 +3,44 @@
 namespace Jenson\City\Helper;
 
 
+use Jenson\City\CityService;
+
 class Helpers
 {
+
+    /**
+     * 获取城市简称
+     *
+     * @param string $cityName
+     * @return array
+     */
+    public static function getCityShortName(string $cityName = ''): array
+    {
+        $city = new CityService();
+        $data = $city->getCityShortName($cityName);
+        return [
+            'cocde' => 200,
+            'data' => $data
+        ];
+    }
+
+    /**
+     * 获取城市信息
+     *
+     * @param int $page
+     * @param int $page_size
+     * @param string $cityName
+     * @return array
+     */
+    public static function getCityAreaInfo(int $page = 1, int $page_size = 10, string $cityName = ''): array
+    {
+        $city = new CityService();
+        $data = $city->getCityAreaInfo($page,$page_size,$cityName);
+        return [
+            'cocde' => 200,
+            'data' => $data
+        ];
+    }
 
 
 }
